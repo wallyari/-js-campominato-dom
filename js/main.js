48333818829126b24d564df41e0d95c2b5d30adb
ariannadepasquale <depasqualearianna@gmail.com>
2022-07-15 05:43:21 +0200

    Add array bombe random

--- js/main.js
+++ js/main.js
@@ -37,12 +37,16 @@ function createNewGame(){
     cellsPerRow = Math.sqrt(cellsNumber);
     
     for ( let i = 1; i <= cellsNumber; i++){
+        //!array bombe random
+        const bombs = generateBombList(16, cellsNumber);
+        console.log(bombs);
         const cell = createSquare(i, cellsPerRow);
         cell.addEventListener('click', function(){
             this.classList.add('clicked');
         });
         document.querySelector('#grid').appendChild(cell);
     }
+
 }
 
 //genera il singolo box
@@ -58,3 +62,54 @@ function createSquare(number, cellsPerRow){
 
 
 };
+
+
+//Il computer deve generare 16 bombe (casuali).
+//Inserisco i 16 numeri generati in un arrey (con un ciclo for).
+//Eventlister rosso=bomba/ nessun variazione (aggiungiamo un punto).
+function generateBombList(bombs, numberOfCells){    
+    const bombList = [];    
+    for(i = 0 ; i < bombs ; i++){      
+        bombList.push(generateUniqueRandomNumber(bombList, 1, numberOfCells));
+    }
+    return bombList;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Function that generates a random number between two included values, which is not already present in the given blacklist.
+ *
+ * @param {*} numsBlacklist The blacklist to check.
+ * @param {*} min The minimum value of the random generated integer.
+ * @param {*} max The maximum value of the random generated integer.
+ * @returns A random generated integer which is not present in the blacklist.
+ */
+function generateUniqueRandomNumber( numsBlacklist, min, max){
+
+    let check = false;
+    let randomInt;
+    while ( !check ){
+
+        randomInt  = Math.floor(Math.random() * ((max + 1) - min) + min);
+        if ( !numsBlacklist.includes(randomInt)  ){
+            check = true;
+        }
+    }
+    return randomInt;
+}
+
